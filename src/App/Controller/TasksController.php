ad134bb9d4b8a1f505e18c4ac95215f2ffe0ddd8
davidjegat <david.jegat@gmail.com>
2013-05-07 15:58:53 +0200

    Just add some controllers for a task management.

---
+++ src/App/Controller/TasksController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+use Knp\RadBundle\Controller\Controller;
+
+class TasksController extends Controller
+{
+	public function indexAction()
+	{
+		return [];
+	}
+}
